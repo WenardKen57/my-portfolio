60f5ca0dbde5ceefc7f6c97ab7dd6812cdd95160
Wenard <wenardken57@gmail.com>
2025-07-23 14:16:14 +0800

    feat: add homepage structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div className="homePage">
       <h1>Home</h1>
+      <section className="introduction"></section>
+      <section className="heroSection"></section>
+      <section className="skills"></section>
+      <section className="links"></section>
     </div>
   );
 }
