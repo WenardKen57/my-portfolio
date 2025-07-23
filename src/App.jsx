518cd597e2e945229dc0dcb98ff30f0941310fe4
Wenard <wenardken57@gmail.com>
2025-07-23 14:13:51 +0800

    feat: add About, Contacts, Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+
 export default function App() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="homePage">
+      <h1>Home</h1>
+    </div>
+  );
+}
