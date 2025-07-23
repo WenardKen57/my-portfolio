4f12581e60eb3c9a45578678e8bd9d9f5944a352
Wenard <wenardken57@gmail.com>
2025-07-23 19:42:31 +0800

    feat: add project links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,16 +14,31 @@ export default function Home() {
         <h3>Projects</h3>
         <ul>
           <li>
-            <a href="" target="__blank">CV Application</a>
+            <a
+              href="https://github.com/WenardKen57/cv-application"
+              target="__blank"
+            >
+              CV Application
+            </a>
             <p>Simple website for building CV applications.</p>
           </li>
           <li>
-            <a href="" target="__blank">Memory game</a>
+            <a href="https://github.com/WenardKen57/memory-game" target="__blank">
+              Memory game
+            </a>
             <p>Remember what you've clicked.</p>
           </li>
           <li>
-            <a href="" target="__blank">Recipe website</a>
-            <p>Want to cook something nice?</p>
+            <a href="https://github.com/WenardKen57/my-to-do-list" target="__blank">
+              JS to do
+            </a>
+            <p>Simple to do list I've made for myself</p>
+          </li>
+          <li>
+            <a href="" target="__blank">
+              Calculator
+            </a>
+            <p>Simple Calculator that can do arithmetic operations</p>
           </li>
         </ul>
       </section>
